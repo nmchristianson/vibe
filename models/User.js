475df7abaf8ce6650c78c1826ceb077428415bed
sshahram <shirin.shahram23@gmail.com>
2021-05-06 11:47:28 -0700

    define User model

---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create user model
+class User extends Model {}
+
+// define table columns and configuration
+User.init(
+    {
+        // define an id column
+        userID: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true 
+        },
+        //define username column
+        userName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        avatar: {
+            type: DataTypes.LONGBLOB,
+        },
+        timeZone: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        favoriteColor: {
+            type: DataTypes.STRING
+        },
+        delFlag: {
+            type: DataTypes.BOOLEAN
+        }
+    },
+    {
+        sequelize,
+        // don't pluralize name of database table
+        freezeTableName: true,
+        // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
+        underscored: true,
+        // make it so our model name stays lowercase in the database
+        modelName: 'user'
+    }
+);
+
+module.exports = User;
