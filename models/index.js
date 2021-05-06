a5e6d57dbdc9cea40277a4a3b216b2b375ab1750
sshahram <shirin.shahram23@gmail.com>
2021-05-06 14:51:01 -0700

    create associations between Mood_Level & Mood

--- models/index.js
+++ models/index.js
@@ -8,11 +8,21 @@ const Log = require('./Log');
 const Login = require('./Login');
 
 // create associations
+
+// between User and Mood
 User.hasMany(Mood, {
     foreignKey: 'userID',
 });
 
 Mood.belongsTo(User, {
-    foreignKey: 'userID'
-})
+    foreignKey: 'userID',
+});
 
+// between Mood and Mood_Level
+Mood_Level.hasMany(Mood, {
+    foreignKey: 'moodLevelID',
+});
+
+Mood.belongsTo(Mood_Level, {
+    foreignKey: 'moodLevelID',
+});
