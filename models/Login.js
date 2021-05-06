f7a4641e0d5cb3de651f6e8b628f67a22631b2b7
sshahram <shirin.shahram23@gmail.com>
2021-05-06 13:42:08 -0700

    hash user's password

--- models/Login.js
+++ models/Login.js
@@ -1,5 +1,13 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+class Login extends Model {
+    // set up method to run on instance data (per user) to check password
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
 
 Login.init(
     {
@@ -7,7 +15,7 @@ Login.init(
             type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
-            autoIncrement: true 
+            autoIncrement: true
         },
         userID: {
             type: DataTypes.INTEGER,
@@ -32,6 +40,18 @@ Login.init(
         }
     },
     {
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            // set up beforeUpdate lifecyle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         sequelize,
         freezeTableName: true,
         underscored: true,
