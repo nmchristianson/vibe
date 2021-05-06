3ff5147a226cf6557d7c91e28fc040b703a34cc3
sshahram <shirin.shahram23@gmail.com>
2021-05-06 14:32:45 -0700

    create session for server

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const sequelize = require('./config/connection');
 
+// session dependencies
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sess = {
+    secret: 'secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(session(sess));
 
 // turn on connection to db and server
 sequelize.sync({force: false}).then(() => {
