122dd096e74a1ddd78a022de999f4e1838f9ad21
sshahram <shirin.shahram23@gmail.com>
2021-05-06 12:08:58 -0700

    define Mood_Level model

---
+++ models/Mood_Level.js
@@ -0,0 +1,31 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Mood_Level extends Model {}
+
+Mood_Level.init(
+    {
+        moodLevelID: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        value: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            unique: true
+        }
+
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'mood_level'
+    }
+);
