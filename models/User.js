ea7f7afd4bddbbbef8777a5438e4f0efc8409350
jypara-git <zhyparaabdieva@gmail.com>
2021-05-09 15:54:33 -0700

    updated user model to include password

--- models/User.js
+++ models/User.js
@@ -1,5 +1,7 @@
-const { Model, DataTypes} = require('sequelize');
+const { Model, DataTypes, TIME} = require('sequelize');
 const sequelize = require('../config/connection');
+const { beforeCreate, beforeUpdate } = require('./Mood');
+const bcrypt = require('bcrypt');
 
 // create user model
 class User extends Model {}
@@ -23,9 +25,31 @@ User.init(
         delFlag: {
             type: DataTypes.BOOLEAN,
             defaultValue: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [4]
+            }
+        },
+        // Unix timestamp for last time user logged in
+        lastLogin: {
+            type: DataTypes.INTEGER,
+            allowNull: true
         }
     },
     {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         sequelize,
         // don't pluralize name of database table
         freezeTableName: true,
